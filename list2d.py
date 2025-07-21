fc958087109650edcd94c124c59efc6eb16647dc
Flint <belajarsmktiairlangga@gmail.com>
2025-07-21 15:28:33 +0800

    menambahkan list2d.py

--- list2d.py
+++ list2d.py
@@ -1,9 +1,9 @@
 nilai = [[10, 20, 30],
          [40, 50, 60],
-         [70, 80, 90]]
+         [70, 80, 90, 100]]
 sum = 0
 for i in range(len(nilai)):
-    for i in range(len(nilai[i])):
+    for j in range(len(nilai[i])):
         sum = sum + nilai[i][j]
     print(sum)
     sum = sum * 0
